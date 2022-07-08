7446cc81c2200664d5bc3de2229bd1bcbf15f9db
anirban_cpp <anirbanbanerjee844@gmail.com>
2022-07-08 23:42:30 +0530

    geolocation

--- src/components/Hooks/Hook.jsx
+++ src/components/Hooks/Hook.jsx
@@ -1,15 +1,26 @@
 import React, { useLayoutEffect, useEffect } from "react";
 
+import useGeoLocation from "../../Hooks/useGeoLocation";
+
 const Hook = () => {
+  const location = useGeoLocation();
+
   useEffect(() => {
     console.log("useEffect");
   }, []);
 
   useLayoutEffect(() => {
     console.log("useLayoutEffect");
-  }, []);   // Called before useEffect
+  }, []); // Called before useEffect
 
-  return <div className="hook"><h1>Hook</h1></div>;
+  return (
+    <div className="hook">
+      <h1>Hook</h1>
+      {location.loaded
+        ? JSON.stringify(location)
+        : "Location not yet available"}
+    </div>
+  );
 };
 
 export default Hook;
